e5e312e2d8e36bea5076292d517c5ef8d0e4a0d3
conicoll <conicoll@ea.com>
2015-12-21 15:23:51 -0800

    Added client layer

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -1,17 +1,30 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name clientApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the clientApp
- */
-angular.module('clientApp')
-  .controller('MainCtrl', function () {
+;
+(function(angular, _, app) {
+  app = angular.module('wormClientApp.controllers.mainController', []);
+
+  app.controller('MainCtrl', [
+    '$scope',
+    'WormService',
+    function($scope, WormService) {
+      $scope.foo = WormService.getHeroById(5);
+      console.log($scope.foo);
+    }
+  ]);
+
+}).call(this, angular, _);
+
+
+
+
+/*
+angular.module('wormClientApp')
+  .controller('MainCtrl', ['WormService', function(WormService) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+  }]);
+*/
