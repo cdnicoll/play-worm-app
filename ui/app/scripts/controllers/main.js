e04250d9310ae7917312fa39200794a789ab1f11
conicoll <conicoll@ea.com>
2016-01-06 21:26:28 -0800

    Setup more of the app FE and layout

--- ui/app/scripts/controllers/main.js
+++ ui/app/scripts/controllers/main.js
@@ -8,10 +8,29 @@
     function($scope, WormService) {
 
       $scope.heroes = [];
+      $scope.selectedHero = null;
+      $scope.showingContent = false;
 
       WormService.getAllHeroes().then(function(heroes) {
         $scope.heroes = heroes;
       });
+
+      $scope.selectHero = function(hero) {
+        $scope.selectedHero = hero;
+      }
+
+/*
+      $scope.showContent = function(hero) { 
+        if(_.isEqual(hero, $scope.selectedHero) && !$scope.showingContent) {
+          $scope.showingContent = true;
+          return true;
+        }
+        else {
+          $scope.showingContent = false;
+          return false;
+        }
+      }
+      */
 
       /*
       WormService.getHeroById(5).then(function(hero) {
