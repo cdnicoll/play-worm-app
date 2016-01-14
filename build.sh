8b9a2f87237dacce616a81cca1ea5264272a00ef
conicoll <conicoll@ea.com>
2016-01-14 15:54:03 -0800

    Fixed things for build

--- build.sh
+++ build.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 cd ui
+echo '------------ NPM Install ------------'
 npm install
+
+echo '------------ bower install ------------'
 bower install
+
+echo '------------ Grunt Force ------------'
 grunt --force
+
+echo '------------ Grunt ------------'
 grunt
+
+echo '------------ Setting up Symlinks ------------'
+cd ..
+ln -s ../../ui/dist/js/ public/javascripts/
+ln -s ../../ui/dist/styles/ public/stylesheets/
 
 # copy the jar
 
