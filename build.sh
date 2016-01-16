1ef6dfecff605099ef2f28ee81e23f8e7c081244
conicoll <conicoll@ea.com>
2016-01-15 22:36:22 -0800

    Update to build script

--- build.sh
+++ build.sh
@@ -13,6 +13,12 @@ bower install
 echo '------------ Grunt Force ------------'
 grunt --force
 
+echo '------------ SBT Setup ------------'
+#sudo -u ubuntu rm -rf /home/ubuntu/.sbt/0.13/staging/
+sbt clean compile
+sbt stage
+
+
 # copy the jar
 
 #chown the jar to www-data:www-data
